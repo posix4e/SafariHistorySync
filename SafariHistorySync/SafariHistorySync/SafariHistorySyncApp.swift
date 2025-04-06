3d858e7fc5d9c1b0cf1c9a7d55b00341d29b12ef
Alex Newman <posix4e@gmail.com>
2025-04-06 07:43:09 -0400

    add xcode

---
+++ SafariHistorySync/SafariHistorySync/SafariHistorySyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafariHistorySyncApp.swift
+//  SafariHistorySync
+//
+//  Created by alex newman on 4/6/25.
+//
+
+import SwiftUI
+
+@main
+struct SafariHistorySyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SafariHistorySync/SafariHistorySyncTests/SafariHistorySyncTests.swift
@@ -0,0 +1,16 @@
+//
+//  SafariHistorySyncTests.swift
+//  SafariHistorySyncTests
+//
+//  Created by alex newman on 4/6/25.
+//
+
+import Testing
+
+struct SafariHistorySyncTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
